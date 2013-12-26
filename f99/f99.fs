1b234d85e9c18f62060d44254874d569e0ebe711
Andrew Smith <smith.andrew.l@gmail.com>
2013-12-26 09:47:01 -0600

    Added comments

--- f99/f99.fs
+++ f99/f99.fs
@@ -1,11 +1,13 @@
 module f99
 
+// F01 (*) Find the last element of a list.
 let rec my_last input = 
     match input with 
     | []           -> None
     | [head]       -> Some(head)
     | head :: tail -> my_last tail
-    
+
+// F02 (*) Find the last but one element of a list.
 let rec next_to_last input =
     match input with
     | []             -> None
@@ -13,6 +15,7 @@ let rec next_to_last input =
     | head :: [last] -> Some(head)
     | head :: tail   -> next_to_last tail
 
+// F03 (*) Find the K'th element of a list.
 let rec element_at input n =
     match (input, n) with
     | ([], _)        -> None
