85198dd278df449ed6f7e7d4614fe509439cdd06
Andrew Smith <smith.andrew.l@gmail.com>
2014-01-02 21:34:02 -0600

    Simplified tests

--- f99/Test.fs
+++ f99/Test.fs
@@ -4,352 +4,255 @@ open NUnit.Framework
 open f99
 
 [<TestFixture>]
-type Test() = 
-        let OneToFive   = [1 .. 5]
-        let OneToFour   = [1 .. 4]
-        let OneToThree  = [1 .. 3]
-        let OneToTwo    = [1 .. 2]
-                
-        let ABCD   = ['a' .. 'd']
-        let XAMAX  = ['x'; 'a'; 'm'; 'a'; 'x']
-     
-        let Dups = ['a'; 'a'; 'a'; 'a'; 'b'; 'c'; 'c'; 
-                    'a'; 'a'; 'd'; 'e'; 'e'; 'e'; 'e']
-        
-        let DupsPacked  = [['a'; 'a'; 'a'; 'a']; ['b']; ['c'; 'c']; 
-                           ['a'; 'a']; ['d']; ['e'; 'e'; 'e'; 'e']]
-                            
-        let DupsEncoded = [(4, 'a'); (1, 'b'); (2, 'c');
-                           (2, 'a'); (1, 'd'); (4, 'e')]
-        
-        [<Test>]
-        abstract F01Last : unit -> unit
-        [<Test>]
-        default this.F01Last () =
-            Assert.AreEqual(Some('d'), my_last ABCD)
-        
-        [<Test>]
-        abstract F01One : unit -> unit
-        [<Test>]
-        default this.F01One () =
-            Assert.AreEqual(Some('a'), my_last ['a'])
+type Test() = class
+    let OneToFive   = [1 .. 5]
+    let OneToFour   = [1 .. 4]
+    let OneToThree  = [1 .. 3]
+    let OneToTwo    = [1 .. 2]
             
-        [<Test>]
-        abstract F01Empty : unit -> unit
-        [<Test>]
-        default this.F01Empty () =
-            Assert.AreEqual(None, my_last [])
-        
-        [<Test>]
-        abstract F02Empty : unit -> unit        
-        [<Test>]
-        default this.F02Empty () =
-            Assert.AreEqual(None, next_to_last [])
-        
-        [<Test>]
-        abstract F02OneElement : unit -> unit
-        [<Test>]
-        default this.F02OneElement () =
-            Assert.AreEqual(None, next_to_last ['a'])
-        
-        [<Test>]
-        abstract F02TwoElements : unit -> unit
-        [<Test>]
-        default this.F02TwoElements () =
-            Assert.AreEqual(Some(1), next_to_last OneToTwo)
-        
-        [<Test>]
-        abstract F02MultipleElements : unit -> unit
-        [<Test>]
-        default this.F02MultipleElements () =
-            Assert.AreEqual(Some(4), next_to_last OneToFive)
-        
-        [<Test>]
-        abstract F03Empty : unit -> unit
-        [<Test>]
-        default this.F03Empty () =
-            Assert.AreEqual(None, element_at [] 0)
-        
-        [<Test>]
-        abstract F03OneElement : unit -> unit
-        [<Test>]
-        default this.F03OneElement () =
-            Assert.AreEqual(Some(1), element_at [1] 0)
-            
-        [<Test>]
-        abstract F03FiveElements : unit -> unit
-        [<Test>]
-        default this.F03FiveElements () =
-            Assert.AreEqual(Some(4), element_at OneToFive 3)
-        
-        [<Test>]
-        abstract F03OutOfBounds : unit -> unit
-        [<Test>]
-        default this.F03OutOfBounds () =
-            Assert.AreEqual(None, element_at OneToFour 5)
-        
-        [<Test>]
-        abstract F04Empty : unit -> unit
-        [<Test>]
-        default this.F04Empty () =
-            Assert.AreEqual(0, num_of_elements [])
+    let ABCD   = ['a' .. 'd']
+    let XAMAX  = ['x'; 'a'; 'm'; 'a'; 'x']
 
-        [<Test>]
-        abstract F04One : unit -> unit
-        [<Test>]
-        default this.F04One () =
-            Assert.AreEqual(1, num_of_elements [1])
-        
-        [<Test>]
-        abstract F04Five: unit -> unit
-        [<Test>]
-        default this.F04Five () =
-            Assert.AreEqual(5, num_of_elements OneToFive)
-        
-        [<Test>]
-        abstract F05Empty: unit -> unit
-        [<Test>]
-        default this.F05Empty () =
-            Assert.AreEqual([], rev [])
-        
-        [<Test>]
-        abstract F05One: unit -> unit
-        [<Test>]
-        default this.F05One () =
-            Assert.AreEqual([2], rev [2])
-            
-        [<Test>]
-        abstract F05Three: unit -> unit
-        [<Test>]
-        default this.F05Three () =
-            Assert.AreEqual([3; 2; 1], rev OneToThree)
-        
-        [<Test>]
-        abstract F06Empty : unit -> unit
-        [<Test>]
-        default this.F06Empty () =
-            Assert.AreEqual(true, is_pal [])
-        
-        [<Test>]
-        abstract F06OneItem : unit -> unit
-        [<Test>]
-        default this.F06OneItem () =
-            Assert.AreEqual(true, is_pal ['a'])
-        
-        [<Test>]
-        abstract F06Pal : unit -> unit
-        [<Test>]
-        default this.F06Pal () =
-            Assert.AreEqual(true, is_pal XAMAX)
-        
-        [<Test>]
-        abstract F06NonPal: unit -> unit
-        [<Test>]
-        default this.F06NonPal () =
-            Assert.AreEqual(false, is_pal ['a' .. 'c'])
-        
-            
-        [<Test>]
-        abstract F08Empty: unit -> unit
-        [<Test>]
-        default this.F08Empty () =
-            Assert.AreEqual([], remove_duplicates [])
-        
-        
-        [<Test>]
-        abstract F08One: unit -> unit
-        [<Test>]
-        default this.F08One () = 
-            Assert.AreEqual([1], remove_duplicates [1])
-            
-        [<Test>]
-        abstract F08TwoDup : unit -> unit
-        [<Test>]
-        default this.F08TwoDup () =
-            Assert.AreEqual([1], remove_duplicates [1; 1])
-        
-        [<Test>]
-        abstract F08ManyDup : unit -> unit
-        [<Test>]
-        default this.F08ManyDup () =
-            Assert.AreEqual(['a'; 'b'; 'c'; 'a'; 'd'; 'e'], remove_duplicates Dups)
-        
-        [<Test>]
-        abstract F09Pack : unit -> unit
-        [<Test>]
-        default this.F09Pack () =
-            Assert.AreEqual(DupsPacked, pack Dups)
-        
-        [<Test>]
-        abstract F10Encode: unit -> unit
-        [<Test>]
-        default this.F10Encode () =
-            Assert.AreEqual(DupsEncoded, encode Dups)
-        
-        [<Test>]
-        abstract F10EncodeOneGroup: unit -> unit
-        [<Test>]
-        default this.F10EncodeOneGroup () =
-            Assert.AreEqual([(5, 'a')], encode ['a'; 'a'; 'a'; 'a'; 'a'])
-            
-        [<Test>]
-        abstract F10EncodeEmpty: unit -> unit
-        [<Test>]
-        default this.F10EncodeEmpty () =
-            Assert.AreEqual(true, List.isEmpty (encode []))
-        
-        [<Test>]
-        abstract F12DecodeEmpty: unit -> unit
-        [<Test>]
-        default this.F12DecodeEmpty () = 
-            Assert.AreEqual(true, List.isEmpty(decode []))
-        
-        [<Test>]
-        abstract F12DecodeDups: unit -> unit
-        [<Test>]
-        default this.F12DecodeDups () =
-            Assert.AreEqual(DupsPacked, decode DupsEncoded)
-            
-        
-        [<Test>]
-        abstract F13EncodeDirect: unit -> unit
-        [<Test>]
-        default this.F13EncodeDirect () =
-            Assert.AreEqual(DupsEncoded, encodeDirect Dups)
-        
-        [<Test>]
-        abstract F13EncodeDirectOneGroup: unit -> unit
-        [<Test>]
-        default this.F13EncodeDirectOneGroup () =
-            Assert.AreEqual([(5, 'a')], encodeDirect ['a'; 'a'; 'a'; 'a'; 'a'])
-            
-        [<Test>]
-        abstract F13EncodeDirectEmpty: unit -> unit
-        [<Test>]
-        default this.F13EncodeDirectEmpty () =
-            Assert.AreEqual(true, List.isEmpty (encodeDirect []))
-            
-        [<Test>]
-        abstract F14DuplicateEmpty: unit -> unit
-        [<Test>]
-        default this.F14DuplicateEmpty () =
-            Assert.AreEqual([], duplic [])
-        
-        [<Test>]
-        abstract F14DuplicateSingle: unit -> unit
-        [<Test>]
-        default this.F14DuplicateSingle () =
-            Assert.AreEqual(['a'; 'a'], duplic ['a'])            
-        
-        [<Test>]
-        abstract F14DuplicateMany: unit -> unit
-        [<Test>]
-        default this.F14DuplicateMany () =
-            let input = ['a'; 'a'; 'b'; 'b'; 
-                         'c'; 'c'; 'd'; 'd']
-            
-            let expected = ['a'; 'a'; 'a'; 'a';
-                            'b'; 'b'; 'b'; 'b';
-                            'c'; 'c'; 'c'; 'c'; 
-                            'd'; 'd'; 'd'; 'd' ]
-            
-            Assert.AreEqual(expected, duplic input)
-        
-        [<Test>]
-        abstract F15DuplicateEmpty: unit -> unit
-        [<Test>]
-        default this.F15DuplicateEmpty () =
-            Assert.AreEqual(true, List.isEmpty (dupli([], 10)))
-        
-        [<Test>]
-        abstract F15DuplicateTwo: unit -> unit
-        [<Test>]
-        default this.F15DuplicateTwo () =
-            let expected = ['a'; 'a'; 'b'; 'b']
-            
-            Assert.AreEqual(expected, dupli (['a'; 'b'], 2))
-        
-        [<Test>]
-        abstract F15DuplicateMany: unit -> unit
-        [<Test>]
-        default this.F15DuplicateMany () =
-        
-            let expected = ['a'; 'a'; 'a'; 
-                            'b'; 'b'; 'b'; 
-                            'c'; 'c'; 'c']
-            
-            Assert.AreEqual(expected, dupli(['a'; 'b'; 'c'], 3))
-        
-        [<Test>]
-        abstract F15DuplicateSeveralZeroTimes : unit -> unit
-        [<Test>]
-        default this.F15DuplicateSeveralZeroTimes () =
-            Assert.AreEqual(true, List.isEmpty(dupli(['a'; 'b'; 'c';], 0)))
-        
-        [<Test>]
-        abstract F16Drop: unit -> unit
-        [<Test>]
-        default this.F16Drop () =
-            Assert.AreEqual(['a'; 'b'; 'd'; 'e'; 'g'; 'h'; 'j'], drop(['a' .. 'j'],3))
+    let Dups = ['a'; 'a'; 'a'; 'a'; 'b'; 'c'; 'c'; 
+                'a'; 'a'; 'd'; 'e'; 'e'; 'e'; 'e']
 
-        [<Test>]
-        abstract F16DropEmpty: unit -> unit
-        [<Test>]
-        default this.F16DropEmpty () =
-            Assert.AreEqual([], drop([], 3))
+    let DupsPacked  = [['a'; 'a'; 'a'; 'a']; ['b']; ['c'; 'c']; 
+                       ['a'; 'a']; ['d']; ['e'; 'e'; 'e'; 'e']]
+                        
+    let DupsEncoded = [(4, 'a'); (1, 'b'); (2, 'c');
+                       (2, 'a'); (1, 'd'); (4, 'e')]
+
+    [<Test>]
+    member self.F01Last () =
+        Assert.AreEqual(Some('d'), my_last ABCD)
+
+    [<Test>]
+    member self.F01One () =
+        Assert.AreEqual(Some('a'), my_last ['a'])
         
-        [<Test>]
-        abstract F16DropEvery: unit -> unit
-        [<Test>]
-        default this.F16DropEvery () =
-            Assert.AreEqual(true, drop(['a' .. 'z'], 1).IsEmpty)
+    [<Test>]
+    member self.F01Empty () =
+        Assert.AreEqual(None, my_last [])
+
+    [<Test>]
+    member self.F02Empty () =
+        Assert.AreEqual(None, next_to_last [])
+
+    [<Test>]
+    member self.F02OneElement () =
+        Assert.AreEqual(None, next_to_last ['a'])
+
+    [<Test>]
+    member self.F02TwoElements () =
+        Assert.AreEqual(Some(1), next_to_last OneToTwo)
+
+    [<Test>]
+    member self.F02MultipleElements () =
+        Assert.AreEqual(Some(4), next_to_last OneToFive)
+
+    [<Test>]
+    member self.F03Empty () =
+        Assert.AreEqual(None, element_at [] 0)
+
+    [<Test>]
+    member self.F03OneElement () =
+        Assert.AreEqual(Some(1), element_at [1] 0)
         
-        [<Test>]
-        abstract F17SplitEmpty: unit -> unit
-        [<Test>]
-        default this.F17SplitEmpty () = 
-            let actual = split([], 0).Value
-            
-            Assert.AreEqual(true, (fst actual).IsEmpty)
-            Assert.AreEqual(true, (snd actual).IsEmpty)
+    [<Test>]
+    member self.F03FiveElements () =
+        Assert.AreEqual(Some(4), element_at OneToFive 3)
+
+    [<Test>]
+    member self.F03OutOfBounds () =
+        Assert.AreEqual(None, element_at OneToFour 5)
+
+    [<Test>]
+    member self.F04Empty () =
+        Assert.AreEqual(0, num_of_elements [])
+
+    [<Test>]
+    member self.F04One () =
+        Assert.AreEqual(1, num_of_elements [1])
+
+    [<Test>]
+    member self.F04Five () =
+        Assert.AreEqual(5, num_of_elements OneToFive)
+
+    [<Test>]
+    member self.F05Empty () =
+        Assert.AreEqual([], rev [])
+
+    [<Test>]
+    member self.F05One () =
+        Assert.AreEqual([2], rev [2])
         
-        [<Test>]
-        abstract F17SplitOneStart: unit -> unit
-        [<Test>]
-        default this.F17SplitOneStart () =
-            let expected = ([], ['a'])
-            let actual   = split(['a'],0).Value
-            
-            Assert.AreEqual(true, (fst actual).IsEmpty)
-            Assert.AreEqual(snd expected, snd actual) 
+    [<Test>]
+    member self.F05Three () =
+        Assert.AreEqual([3; 2; 1], rev OneToThree)
+
+    [<Test>]
+    member self.F06Empty () =
+        Assert.AreEqual(true, is_pal [])
+
+    [<Test>]
+    member self.F06OneItem () =
+        Assert.AreEqual(true, is_pal ['a'])
+
+    [<Test>]
+    member self.F06Pal () =
+        Assert.AreEqual(true, is_pal XAMAX)
+
+    [<Test>]
+    member self.F06NonPal () =
+        Assert.AreEqual(false, is_pal ['a' .. 'c'])
+
         
-        [<Test>]
-        abstract F17SplitOneEnd: unit -> unit
-        [<Test>]
-        default this.F17SplitOneEnd () =
-            let expected = (['a'], [])
-            let actual   = split(['a'],1).Value
-            
-            Assert.AreEqual(fst   expected, fst actual)
-            Assert.AreEqual(true, (snd actual).IsEmpty)
-            
-        [<Test>]
-        abstract F17SplitTwoMiddle: unit -> unit
-        [<Test>]
-        default this.F17SplitTwoMiddle () =
-            let expected = (['a'], ['b'])
-            let actual   = split(['a'; 'b'], 1).Value
-            
-            Assert.AreEqual(fst expected, fst actual)
-            Assert.AreEqual(snd expected, snd actual)
+    [<Test>]
+    member self.F08Empty () =
+        Assert.AreEqual([], remove_duplicates [])
+
+
+    [<Test>]
+    member self.F08One () = 
+        Assert.AreEqual([1], remove_duplicates [1])
         
-        [<Test>]
-        abstract F17SplitFiveAtThree: unit -> unit
-        [<Test>]
-        default this.F17SplitFiveAtThree() =
-            let expected = (['a' .. 'c'], ['d'; 'e'])
-            let actual   = split(['a' .. 'e'], 3).Value
-            
-            Assert.AreEqual(fst expected, fst actual)
-            Assert.AreEqual(snd expected, snd expected)
+    [<Test>]
+    member self.F08TwoDup () =
+        Assert.AreEqual([1], remove_duplicates [1; 1])
+
+    [<Test>]
+    member self.F08ManyDup () =
+        Assert.AreEqual(['a'; 'b'; 'c'; 'a'; 'd'; 'e'], remove_duplicates Dups)
+
+    [<Test>]
+    member self.F09Pack () =
+        Assert.AreEqual(DupsPacked, pack Dups)
+
+    [<Test>]
+    member self.F10Encode () =
+        Assert.AreEqual(DupsEncoded, encode Dups)
+
+    [<Test>]
+    member self.F10EncodeOneGroup () =
+        Assert.AreEqual([(5, 'a')], encode ['a'; 'a'; 'a'; 'a'; 'a'])
+        
+    [<Test>]
+    member self.F10EncodeEmpty () =
+        Assert.AreEqual(true, List.isEmpty (encode []))
+
+    [<Test>]
+    member self.F12DecodeEmpty () = 
+        Assert.AreEqual(true, List.isEmpty(decode []))
+
+    [<Test>]
+    member self.F12DecodeDups () =
+        Assert.AreEqual(DupsPacked, decode DupsEncoded)
+        
+
+    [<Test>]
+    member self.F13EncodeDirect () =
+        Assert.AreEqual(DupsEncoded, encodeDirect Dups)
+
+    [<Test>]
+    member self.F13EncodeDirectOneGroup () =
+        Assert.AreEqual([(5, 'a')], encodeDirect ['a'; 'a'; 'a'; 'a'; 'a'])
+        
+    [<Test>]
+    member self.F13EncodeDirectEmpty () =
+        Assert.AreEqual(true, List.isEmpty (encodeDirect []))
+        
+    [<Test>]
+    member self.F14DuplicateEmpty () =
+        Assert.AreEqual([], duplic [])
+
+    [<Test>]
+    member self.F14DuplicateSingle () =
+        Assert.AreEqual(['a'; 'a'], duplic ['a'])            
+
+    [<Test>]
+    member self.F14DuplicateMany () =
+        let input = ['a'; 'a'; 'b'; 'b'; 
+                     'c'; 'c'; 'd'; 'd']
+        
+        let expected = ['a'; 'a'; 'a'; 'a';
+                        'b'; 'b'; 'b'; 'b';
+                        'c'; 'c'; 'c'; 'c'; 
+                        'd'; 'd'; 'd'; 'd' ]
+        
+        Assert.AreEqual(expected, duplic input)
+
+    [<Test>]
+    member self.F15DuplicateEmpty () =
+        Assert.AreEqual(true, List.isEmpty (dupli([], 10)))
+
+    [<Test>]
+    member self.F15DuplicateTwo () =
+        let expected = ['a'; 'a'; 'b'; 'b']
+        
+        Assert.AreEqual(expected, dupli (['a'; 'b'], 2))
+
+    [<Test>]
+    member self.F15DuplicateMany () =
+
+        let expected = ['a'; 'a'; 'a'; 
+                        'b'; 'b'; 'b'; 
+                        'c'; 'c'; 'c']
+        
+        Assert.AreEqual(expected, dupli(['a'; 'b'; 'c'], 3))
+
+    [<Test>]
+    member self.F15DuplicateSeveralZeroTimes () =
+        Assert.AreEqual(true, List.isEmpty(dupli(['a'; 'b'; 'c';], 0)))
+
+    [<Test>]
+    member self.F16Drop () =
+        Assert.AreEqual(['a'; 'b'; 'd'; 'e'; 'g'; 'h'; 'j'], drop(['a' .. 'j'],3))
+
+    [<Test>]
+    member self.F16DropEmpty () =
+        Assert.AreEqual([], drop([], 3))
+
+    [<Test>]
+    member self.F16DropEvery () =
+        Assert.AreEqual(true, drop(['a' .. 'z'], 1).IsEmpty)
+
+    [<Test>]
+    member self.F17SplitEmpty () = 
+        let actual = split([], 0).Value
+        
+        Assert.AreEqual(true, (fst actual).IsEmpty)
+        Assert.AreEqual(true, (snd actual).IsEmpty)
+
+    [<Test>]
+    member self.F17SplitOneStart () =
+        let expected = ([], ['a'])
+        let actual   = split(['a'],0).Value
+        
+        Assert.AreEqual(true, (fst actual).IsEmpty)
+        Assert.AreEqual(snd expected, snd actual) 
+
+    [<Test>]
+    member self.F17SplitOneEnd () =
+        let expected = (['a'], [])
+        let actual   = split(['a'],1).Value
+        
+        Assert.AreEqual(fst   expected, fst actual)
+        Assert.AreEqual(true, (snd actual).IsEmpty)
+        
+    [<Test>]
+    member self.F17SplitTwoMiddle () =
+        let expected = (['a'], ['b'])
+        let actual   = split(['a'; 'b'], 1).Value
+        
+        Assert.AreEqual(fst expected, fst actual)
+        Assert.AreEqual(snd expected, snd actual)
+
+    [<Test>]
+    member self.F17SplitFiveAtThree() =
+        let expected = (['a' .. 'c'], ['d'; 'e'])
+        let actual   = split(['a' .. 'e'], 3).Value
+        
+        Assert.AreEqual(fst expected, fst actual)
+        Assert.AreEqual(snd expected, snd expected)
+end
        
