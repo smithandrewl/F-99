9b8a7e8eed62f8d7e1c23c89e4c64caf4137df3c
Andrew Smith <smith.andrew.l@gmail.com>
2014-04-14 22:17:40 -0500

    Added BinaryTrees.fs for problems 54 to 69

---
+++ f99/BinaryTrees.fs
@@ -0,0 +1,14 @@
+﻿namespace F99
+
+module BinaryTrees =
+    type BinaryTree<'a> =
+        | Node of value: 'a * left: BinaryTree<'a> * right: BinaryTree<'a>
+        | Leaf of value: 'a
+
+    (*
+        P54 (*) Write a predicate istree which succeeds if and only if 
+                its argument is a binary tree.  Not necessary since F# is strongly typed
+    *)
+
+
+            
